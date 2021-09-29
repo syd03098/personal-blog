657715bd1820de0c2e0fb3165020f6215a9c4d7c
yonghokim <dektitu@gmail.com>
2021-09-29 22:07:57 +0900

    new timestamp components

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -12,9 +12,23 @@ export const parseImageUrl = (block: ImageBlock): string => {
   return notionImageUrl.toString();
 };
 
+export const parsePublishedTime = (date: number): string => {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+};
+
 export const parseTimeFromNow = (date: number): string => {
   const seconds = Math.floor((Date.now() - date) / 1000);
   const interval = timeIntervals.find((value) => value.seconds < seconds);
   const count = Math.floor(seconds / interval.seconds);
-  return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
+
+  switch (interval.label) {
+    case 'just a minutes ago':
+      return `${interval.label}`;
+    default:
+      return `${count} ${interval.label} ago`;
+  }
 };
